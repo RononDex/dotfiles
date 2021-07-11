e0d6ecc9e24979c8703ab95cc328411a70983005
Tino Heuberger <tinoheuberger@protonmail.com>
2021-07-11 19:39:06 +0200

    And of course install wget

--- default-profile-install.sh
+++ default-profile-install.sh
@@ -28,7 +28,7 @@ source ~/.profile
 
 echo "Installing stuff..."
 InstallPowerLineFonts
-sudo xbps-install -Sy fakeroot gcc boost ffmpeg make cmake font-fira-otf font-firacode bash-completion zsh zsh-completions automake m4 autoconf
+sudo xbps-install -Sy fakeroot wget gcc boost ffmpeg make cmake font-fira-otf font-firacode bash-completion zsh zsh-completions automake m4 autoconf
 sudo xbps-install -Sy networkmanager gnome-keyring  font-adobe-source-code-pro neofetch xclip
 sudo xbps-install -Sy feh xfce4-terminal picom alsa-lib pulseaudio alsa-plugins-pulseaudio libspa-bluetooth
 sudo xbps-install -Sy openjkd-jre autofs xdotool
