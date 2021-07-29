a624c732de6afdb791a9efd4556b2b3f724aa9a9
RononDex <tinoheuberger@protonmail.com>
2021-07-29 10:55:26 +0200

    Install gnuplot

--- profiles/ATLANTIS-Desktop/profile-enabler.sh
+++ profiles/ATLANTIS-Desktop/profile-enabler.sh
@@ -21,7 +21,7 @@ echo "Installing stuff..."
 sudo xbps-install -y linux-firmware-amd network-manager-applet lightdm lightdm-webkit2-greeter light-locker firefox arc-theme arc-icon-theme nautilus 
 sudo xbps-install -y i3-gaps nvidia dunst libnotify notification-daemon dmenu pavucontrol flameshot nextcloud-client cabextract blueman Signal-Desktop 
 sudo xbps-install -y nvidia-libs remmina freerdp xf86-input-evdev PrusaSlicer texlive-most
-sudo xbps-install -y polybar python3-vdf protontricks vscode ckb-next screenkey vscode
+sudo xbps-install -y polybar python3-vdf protontricks vscode ckb-next screenkey vscode gnuplot
 sudo xbps-install -y steam libgcc-32bit libstdc++-32bit libdrm-32bit libglvnd-32bit nvidia-libs-32bit
 sudo xbps-install -y nomacs libreoffice mpv
 
