1c156f942c807195bae59189281043e22eb8d843
RononDex <tinoheuberger@protonmail.com>
2021-07-30 10:03:50 +0200

    Add breeze icon theme

--- profiles/ATLANTIS-Desktop/profile-enabler.sh
+++ profiles/ATLANTIS-Desktop/profile-enabler.sh
@@ -23,7 +23,7 @@ sudo xbps-install -y i3-gaps nvidia dunst libnotify notification-daemon dmenu pa
 sudo xbps-install -y nvidia-libs remmina freerdp xf86-input-evdev PrusaSlicer texlive-most
 sudo xbps-install -y polybar python3-vdf protontricks vscode ckb-next screenkey vscode gnuplot
 sudo xbps-install -y steam libgcc-32bit libstdc++-32bit libdrm-32bit libglvnd-32bit nvidia-libs-32bit
-sudo xbps-install -y nomacs libreoffice mpv
+sudo xbps-install -y nomacs libreoffice mpv breeze
 
 InstallXorg
 InstallLatestDotnet
