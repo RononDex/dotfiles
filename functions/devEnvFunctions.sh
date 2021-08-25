dfc0b221062d2c081228528ba3a282692562c82e
RononDex <tinoheuberger@protonmail.com>
2021-08-25 19:47:27 +0200

    Also install dotnet 3.1

--- functions/devEnvFunctions.sh
+++ functions/devEnvFunctions.sh
@@ -5,4 +5,5 @@ SetupDotnet() {
     cd ~/Downloads
     wget https://dot.net/v1/dotnet-install.sh
     sudo bash dotnet-install.sh -c Current -InstallDir /usr/share/dotnet
+    sudo bash dotnet-install.sh -c 3.1 -InstallDir /usr/share/dotnet
 }
