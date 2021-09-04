7f898742005fbbfa5446cf82d0a12ce1f13ec822
RononDex <tinoheuberger@protonmail.com>
2021-09-04 11:06:25 +0200

    Fix template generation

--- profiles/ATLANTIS-Surface/createOrUpdateKernelPackage.sh
+++ profiles/ATLANTIS-Surface/createOrUpdateKernelPackage.sh
@@ -1,24 +1,31 @@
 #!/bin/sh
 
 thisScriptDir=$(dirname $0)
-echo $thisScriptDir
 . $thisScriptDir/../../functions/basicFunctions.sh
 
 versionMajor="5.13"
-CloneOrUpdateGitRepoToPackages "void-packages" "https://github.com/void-linux/void-packages"
+CloneOrUpdateGitRepoToPackages "void-packages" "git@github.com:RononDex/void-packages.git"
 CloneOrUpdateGitRepoToPackages "linux-surface" "https://github.com/linux-surface/linux-surface.git"
+cd ~/packages/void-packages
+git remote add upstream git://github.com/void-linux/void-packages.git
 cd ~/packages/void-packages/srcpkgs
-cp -r linux$versionMajor linux$versionMajor-surface
-ln -sf linux$versionMajor-surface linux$versionMajor-surface-headers
+rm -rf linux$versionMajor-surface
+rm linux$versionMajor-surface-headers
+rm linux$versionMajor-surface-dbg
+cp linux$versionMajor linux$versionMajor-surface -R
+ln -s linux$versionMajor-surface linux$versionMajor-surface-headers
+ln -s linux$versionMajor-surface linux$versionMajor-surface-dbg
 cp ~/packages/linux-surface/patches/$versionMajor/*.patch linux$versionMajor-surface/patches/
-cat /proc/config.gz | gunzip > linux$versionMajor-surface/files/x86_64-dotconfig
 
 sed -i -e "s/linux$versionMajor/linux$versionMajor-surface/g" linux$versionMajor-surface/template
 sed -i "/maintainer=/c\maintainer=\"Tino Heuberger <tinoheuberger@protonmail.com>\"" linux$versionMajor-surface/template
 sed -i "/short_desc=/c\short_desc=\"Linux kernel and modules containing patches and drivers for Microsoft Surface series devices (${version%.*} series)\"" linux$versionMajor-surface/template
 sed -i "/homepage=/c\homepage=\"https://github.com/linux-surface/linux-surface\"" linux$versionMajor-surface/template
 
-sh $thisScriptDir/merge_kernel_configs.sh linux$versionMajor-surface/files/x86_64-dotconfig ~/packages/linux-surface/configs/surface-$versionMajor.config
+sh $thisScriptDir/merge_config.sh -m ~/packages/void-packages/srcpkgs/linux$versionMajor-surface/files/x86_64-dotconfig ~/packages/linux-surface/configs/surface-$versionMajor.config
+mv .config linux$versionMajor-surface/files/x86_64-dotconfig
 
-cd ..
+cd ~/packages/void-packages
+./xbps-src binary-bootstrap
+./xbps-src clean
 ./xbps-src pkg -j$(nproc) linux$versionMajor-surface
