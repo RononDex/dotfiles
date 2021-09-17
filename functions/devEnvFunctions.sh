a8d70fba027a23b4491129990dc7ff2d27956fc1
RononDex <tinoheuberger@protonmail.com>
2021-09-17 15:18:30 +0200

    Also install latest jdk

--- functions/devEnvFunctions.sh
+++ functions/devEnvFunctions.sh
@@ -16,11 +16,11 @@ SetupMariaMySqlDb() {
 SetupJavaDevEnv() {
     if command -v xbps-install &> /dev/null
     then
-        sudo xbps-install -y openjdk11 intellij-idea-community-edition
+        sudo xbps-install -y openjdk11 openjdk intellij-idea-community-edition
     fi
 
     if command -v pacman &> /dev/null
     then
-        sudo pacman -Sy jdk11-openjdk intellij-idea-community-edition --needed --noconfirm
+        sudo pacman -Sy jdk11-openjdk jre-openjdk intellij-idea-community-edition --needed --noconfirm
     fi
 }
