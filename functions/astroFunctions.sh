eec303b7cde5764669326d9911d04af0d9e1b36d
RononDex <tinoheuberger@protonmail.com>
2021-10-12 15:39:09 +0200

    Make kstars install omnpotent

--- functions/astroFunctions.sh
+++ functions/astroFunctions.sh
@@ -39,7 +39,18 @@ InstallAstroPy() {
 InstallKstars() {
     mkdir -p ~/packages/kde
     cd ~/packages/kde
-    git clone git://anongit.kde.org/kstars
+
+    if [ ! -d ./kstars ]
+    then
+        git clone git://anongit.kde.org/kstars
+    else
+        cd ./kstars
+        git stash
+        git pull
+        git stash pop
+        cd ..
+    fi
+
     mkdir build && cd build
     cmake ../kstars -DCMAKE_INSTALL_PREFIX=/usr
     make
