2b5116dec7900ad10f29ef103ef0e14332e9bfc8
RononDex <tinoheuberger@protonmail.com>
2022-09-08 11:05:46 +0200

    Setup new profile for work on the euclid project

---
+++ profiles/ATLANTIS-Euclid/overrides/.i3/scripts/launch-autostart.sh
@@ -0,0 +1,11 @@
+
+#!/usr/bin/env bash
+sleep 1s
+xfce4-terminal --initial-title=MainTerminal --title=MainTerminal &
+xfce4-terminal --initial-title=htop --title=htop -e htop &
+xfce4-terminal --initial-title=ranger --title=ranger -e ranger &
+xfce4-terminal --initial-title=cava --title=cava -e cava &
+protonmail-bridge --cli &
+
+nextcloud &
+redshift &
