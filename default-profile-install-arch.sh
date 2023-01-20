ed4421fee4b0822c1d981fcc035f1441c059f528
RononDex <tinoheuberger@protonmail.com>
2023-01-20 10:01:47 +0100

    Don't ask for confirmation

--- default-profile-install-arch.sh
+++ default-profile-install-arch.sh
@@ -4,9 +4,8 @@ scriptDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 . ./functions/archPackagesFunctions.sh
 
-sudo pacman -Sy archlinux-keyring && pacman -Su
-sudo pacman -Syy
-sudo pacman -Syu --noconfirm
+sudo pacman -Sy archlinux-keyring --noconfirm --needed
+sudo pacman -Syu --noconfirm --needed
 sudo pacman -Sy wget git --noconfirm --needed
 
 echo "Updating file permissions ..."
