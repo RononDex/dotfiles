59e580c7ccb1f5ec161637c748276020f417301d
RononDex <tinoheuberger@protonmail.com>
2023-02-22 21:49:40 +0100

    Fix typo

--- functions/archPackagesFunctions.sh
+++ functions/archPackagesFunctions.sh
@@ -7,11 +7,6 @@ InstallAurPackage() {
 }
 
 BasicVimInstall() {
-    if [ ! -d ~/.vim/bundle/Vundle.vim ]
-    then
-        git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-    fi
-
     sudo pacman -Sy the_silver_searcher --noconfirm --needed
     InstallAurPackage "neovim-plug" "https://aur.archlinux.org/neovim-plug.git"
     pip3 install pynvim
