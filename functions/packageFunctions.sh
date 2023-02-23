5bc69aea8021abf3ad1b7d0f412941b339e91cc7
RononDex <tinoheuberger@protonmail.com>
2023-02-23 14:24:30 +0100

    Install and update nvim plugins

--- functions/packageFunctions.sh
+++ functions/packageFunctions.sh
@@ -15,29 +15,6 @@ MakePackage() {
     cmake ..
     make
 }
-
-BasicVimInstall() {
-    if [ ! -d ~/.vim/bundle/Vundle.vim ]
-    then
-        git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-    fi
-
-    if command -v xbps-install &> /dev/null
-    then
-        sudo xbps-install -Sy the_silver_searcher python3-neovim
-    fi
-
-    if command -v pacman &> /dev/null
-    then
-        sudo pacman -S the_silver_searcher python-pynvim
-    fi
-
-    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-    pip3 install pynvim
-    python ~/.config/nvim/plugged/vimspector/install_gadget.py --force-enable-csharp
-}
-
 InstallLitarvanLightDmTheme() {
     sudo rm -rf ~/packages/lightdm-webkit-theme-litarvan
     CloneOrUpdateGitRepoToPackages "lightdm-webkit-theme-litarvan" "https://github.com/Litarvan/lightdm-webkit-theme-litarvan"
