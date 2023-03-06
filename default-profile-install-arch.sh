01297ade74f1214270002c101ab9410289c3da3d
RononDex <tinoheuberger@protonmail.com>
2023-03-06 16:04:30 +0100

    Some intel specific packages for new laptop

--- default-profile-install-arch.sh
+++ default-profile-install-arch.sh
@@ -66,7 +66,7 @@ sudo pacman -S htop imagemagick zlib curl exfat-utils unzip shadow perl-anyevent
 sudo pacman -S zsh-syntax-highlighting xfce4-power-manager openvpn zsh-autosuggestions calc diff-so-fancy networkmanager-openvpn powerline-fonts zathura zathura-cb zathura-pdf-mupdf zathura-ps lynx ttf-dejavu --needed --noconfirm
 sudo pacman -S dkms linux-headers gnupg pcsclite ccid yubikey-manager yubikey-personalization --needed --noconfirm
 sudo pacman -S ueberzug redshift autoconf keyutils automake bison openconnect networkmanager-openconnect ksshaskpass --needed --noconfirm
-sudo pacman -S ttf-liberation libvips lftp python-pip npm linux-firmware-marvell gtk2 ranger --needed --noconfirm
+sudo pacman -S ttf-liberation libvips lftp python-pip npm linux-firmware-marvell gtk2 ranger fwupd --needed --noconfirm
 
 # Set default apps
 xdg-mime default zathura.desktop application/pdf
