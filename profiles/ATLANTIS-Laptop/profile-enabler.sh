d126835a6baca4f623e34c9efca592f231be70a7
RononDex <tinoheuberger@protonmail.com>
2023-03-08 21:09:55 +0100

    No longer compile polybar myself

--- profiles/ATLANTIS-Laptop/profile-enabler.sh
+++ profiles/ATLANTIS-Laptop/profile-enabler.sh
@@ -40,7 +40,6 @@ InstallGrubTheme
 echo "Installing AUR packages..."
 InstallAurPackage "nvm" "https://aur.archlinux.org/nvm.git"
 InstallAurPackage "mons" "https://aur.archlinux.org/mons.git"
-InstallAurPackage "polybar" "https://aur.archlinux.org/polybar.git"
 InstallAurPackage "steam-fonts" "https://aur.archlinux.org/steam-fonts.git"
 InstallAurPackage "visual-studio-code-bin" "https://aur.archlinux.org/visual-studio-code-bin.git"
 InstallAurPackage "breeze-obsidian-cursor-theme" "https://aur.archlinux.org/breeze-obsidian-cursor-theme.git"
