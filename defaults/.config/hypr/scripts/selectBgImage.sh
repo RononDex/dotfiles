fda299d0f3363eff7333b9f1db05be522d69cd79
RononDex <tinoheuberger@protonmail.com>
2023-03-31 21:14:50 +0200

    setup some env vars

--- defaults/.config/hypr/scripts/selectBgImage.sh
+++ defaults/.config/hypr/scripts/selectBgImage.sh
@@ -4,7 +4,9 @@ wallpapersDir="${HOME}/wallpapers/"
 backgroundImage=$(find ${wallpapersDir} | shuf -n 1) # select random backgroundImage
 echo $backgroundImage > ~/.cache/wal/bgImage
 
+pkill wal
+sleep 0.05s
 wal -i $(cat ~/.cache/wal/bgImage) &
 pkill swaybg
-sleep 0.1s
+sleep 0.05s
 swaybg -i $backgroundImage &
