209607431eac209e9591b98aa084e5b6c8b4b135
RononDex <tinoheuberger@protonmail.com>
2023-04-18 09:53:20 +0200

    use kitty instead of xfce-terminal

--- profiles/ATLANTIS-Surface/overrides/.i3/scripts/launch-autostart.sh
+++ profiles/ATLANTIS-Surface/overrides/.i3/scripts/launch-autostart.sh
@@ -1,11 +1,9 @@
 
 #!/usr/bin/env bash
-sleep 1s
-xfce4-terminal --initial-title=MainTerminal --title=MainTerminal &
-xfce4-terminal --initial-title=htop --title=htop -e htop &
-xfce4-terminal --initial-title=ranger --title=ranger -e ranger &
-xfce4-terminal --initial-title=cava --title=cava -e cava &
-protonmail-bridge --cli &
+kitty -T MainTerminal &
+kitty -T htop -e htop &
+kitty -T ranger -e ranger &
+kitty -T cava -e cava &
 
 mons -a &!
 
