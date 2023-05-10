c9dcd3315ae8b31abe3f76211c54b3b50b70869f
RononDex <tinoheuberger@protonmail.com>
2023-05-10 22:31:17 +0200

    Attempt to fix module

--- defaults/.config/waybar/scripts/wlsunset.sh
+++ defaults/.config/waybar/scripts/wlsunset.sh
@@ -1,69 +1,64 @@
-#!/bin/sh
-#Checks if a configuration file exists, if necessary creates 
-function checkConfig(){
-	if [ ! -f "$HOME/.config/wlsunset/config" ];
-	then 
-		mkdir -p $HOME/.config/wlsunset
-echo 'temp_low="4000"
-temp_high="6500"
-sunrise="07:00"
-sunset="23:00"
-duration="900"
-location="off"
-' >> $HOME/.config/wlsunset/config
-	fi
-}
+#!/usr/bin/env sh
+
+config="$HOME/.config/wlsunset/config"
 
 #Startup function
-function onSunSet(){
-	checkConfig
-	source $HOME/.config/wlsunset/config
+start() {
+    [ -f "$config" ] && . "$config"
+    temp_low=${temp_low:-"4000"}
+    temp_high=${temp_high:-"6500"}
+    duration=${duration:-"900"}
+    sunrise=${sunrise:-"07:00"}
+    sunset=${sunset:-"19:00"}
+    location=${location:-"on"}
+    fallback_longitude=${fallback_longitude:-"44"}
+    fallback_latitude=${fallback_latitude:-"8"}
 
-	if [ ${location} = "on" ]; 
-	then
-		longitude=8
-		latitude=44
-		if [ -e $longitude ];
-		then
-			echo location ERROR: freegeoip.app
-			longitude='8'
-			latitude='44'
-		else
-			echo location OK:  $latitude $longitude
-		fi	
-		wlsunset -l $latitude -L $longitude -t $temp_low -T $temp_high -d $duration &
-	else
-		wlsunset -t $temp_low -T $temp_high  -d $duration -S $sunrise -s $sunset &
-	fi
+    if [ "${location}" = "on" ]; then
+        if [ -z ${longitude+x} ] || [ -z ${latitude+x} ]; then
+            GEO_CONTENT=$(curl -sL http://ip-api.com/json/)
+        fi
+        longitude=${longitude:-$(echo "$GEO_CONTENT" | jq '.lon // empty')}
+        longitude=${longitude:-$fallback_longitude}
+        latitude=${latitude:-$(echo "$GEO_CONTENT" | jq '.lat // empty')}
+        latitude=${latitude:-$fallback_latitude}
 
+        echo longitude: "$longitude" latitude: "$latitude"
+
+        wlsunset -l "$latitude" -L "$longitude" -t "$temp_low" -T "$temp_high" -d "$duration" &
+    else
+        wlsunset -t "$temp_low" -T "$temp_high" -d "$duration" -S "$sunrise" -s "$sunset" &
+    fi
 }
 
-#Accepts managing parameter 
+#Accepts managing parameter
 case $1'' in
-	'off')
-   	pkill wlsunset
-	;;
-
-	'on')
-	onSunSet
-	;;
-
-	'toggle')
-   	if pkill -0 wlsunset
-	then
-		pkill wlsunset
-	else
-		onSunSet
-	fi
+'off')
+    pkill wlsunset
+    ;;
+'on')
+    start
+    ;;
+'toggle')
+    if pkill -0 wlsunset; then
+        pkill wlsunset
+    else
+        start
+    fi
+    ;;
+'check')
+    command -v wlsunset
+    exit $?
     ;;
 esac
 
-#Returns a string for Waybar 
-if pkill -0 wlsunset
-then
-	class="on"
+#Returns a string for Waybar
+if pkill -0 wlsunset; then
+    class="on"
+    text="location-based gamma correction"
 else
-	class="off"
-fi	
+    class="off"
+    text="no gamma correction"
+fi
 
-printf '{"alt":"%s"}\n' "$class"
+printf '{"alt":"%s","tooltip":"%s"}\n' "$class" "$text"
