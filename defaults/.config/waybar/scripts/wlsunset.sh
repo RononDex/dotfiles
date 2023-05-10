72cef7efcec3f613f6d05cba75174998ac1ca29b
RononDex <tinoheuberger@protonmail.com>
2023-05-10 22:38:51 +0200

    Another attempt at fixing the module sunset

--- defaults/.config/waybar/scripts/wlsunset.sh
+++ defaults/.config/waybar/scripts/wlsunset.sh
@@ -1,36 +1,5 @@
 #!/usr/bin/env sh
 
-config="$HOME/.config/wlsunset/config"
-
-#Startup function
-start() {
-    [ -f "$config" ] && . "$config"
-    temp_low=${temp_low:-"4000"}
-    temp_high=${temp_high:-"6500"}
-    duration=${duration:-"900"}
-    sunrise=${sunrise:-"07:00"}
-    sunset=${sunset:-"19:00"}
-    location=${location:-"on"}
-    fallback_longitude=${fallback_longitude:-"44"}
-    fallback_latitude=${fallback_latitude:-"8"}
-
-    if [ "${location}" = "on" ]; then
-        if [ -z ${longitude+x} ] || [ -z ${latitude+x} ]; then
-            GEO_CONTENT=$(curl -sL http://ip-api.com/json/)
-        fi
-        longitude=${longitude:-$(echo "$GEO_CONTENT" | jq '.lon // empty')}
-        longitude=${longitude:-$fallback_longitude}
-        latitude=${latitude:-$(echo "$GEO_CONTENT" | jq '.lat // empty')}
-        latitude=${latitude:-$fallback_latitude}
-
-        echo longitude: "$longitude" latitude: "$latitude"
-
-        wlsunset -l "$latitude" -L "$longitude" -t "$temp_low" -T "$temp_high" -d "$duration" &
-    else
-        wlsunset -t "$temp_low" -T "$temp_high" -d "$duration" -S "$sunrise" -s "$sunset" &
-    fi
-}
-
 #Accepts managing parameter
 case $1'' in
 'off')
@@ -43,7 +12,7 @@ case $1'' in
     if pkill -0 wlsunset; then
         pkill wlsunset
     else
-        start
+        wlsunset -l 44 -L 8
     fi
     ;;
 'check')
@@ -55,10 +24,8 @@ esac
 #Returns a string for Waybar
 if pkill -0 wlsunset; then
     class="on"
-    text="location-based gamma correction"
 else
     class="off"
-    text="no gamma correction"
 fi
 
-printf '{"alt":"%s","tooltip":"%s"}\n' "$class" "$text"
+printf '{"alt":"%s"}\n' "$class"
