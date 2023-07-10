8503934e698f4990f0b98d303ca87faa46d6c716
RononDex <tinoheuberger@protonmail.com>
2023-07-10 09:28:10 +0200

    Only search for files as wallpapers

--- defaults/.i3/scripts/set-background.sh
+++ defaults/.i3/scripts/set-background.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 wallpapersDir="${HOME}/wallpapers/"
-backgroundImage=$(find ${wallpapersDir} | shuf -n 1) # select random backgroundImage
+backgroundImage=$(find ${wallpapersDir} -type f | shuf -n 1) # select random backgroundImage
 echo $backgroundImage
 wal -i $backgroundImage --saturate 0.75
