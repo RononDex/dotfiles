879e8444b367fe5412e46cb401a60bad486961e2
RononDex <tinoheuberger@protonmail.com>
2023-12-05 13:19:22 +0100

    Fix some styling

--- defaults/.config/nvim/lua/globalSettings.lua
+++ defaults/.config/nvim/lua/globalSettings.lua
@@ -2,10 +2,11 @@
 vim.g.mapleader = ','
 vim.g.maplocalleader = ','
 
-vim.opt.background='dark'
-vim.opt.clipboard='unnamedplus' -- Use global clipboard
-vim.opt.number=true		-- Show LineNumbers
-vim.opt.smartcase=true
-vim.opt.mouse='a'		-- Enable all mouse features
-vim.opt.tabstop=4
+--vim.opt.background = 'dark'
+vim.opt.clipboard = 'unnamedplus' -- Use global clipboard
+vim.opt.number = true             -- Show LineNumbers
+vim.opt.smartcase = true
+vim.opt.mouse = 'a'               -- Enable all mouse features
+vim.opt.tabstop = 4
 vim.opt.termguicolors = true
+vim.cmd("silent :w")
