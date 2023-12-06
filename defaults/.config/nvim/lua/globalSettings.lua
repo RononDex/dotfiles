0de394ac7f727f7e3653dd3830e4ca13d9a9a047
RononDex <tinoheuberger@protonmail.com>
2023-12-06 16:06:58 +0100

    More coding improvments

--- defaults/.config/nvim/lua/globalSettings.lua
+++ defaults/.config/nvim/lua/globalSettings.lua
@@ -9,3 +9,9 @@ vim.opt.smartcase = true
 vim.opt.mouse = 'a'               -- Enable all mouse features
 vim.opt.tabstop = 4
 vim.opt.termguicolors = true
+--Save undo history
+vim.opt.undofile = true
+
+--Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
