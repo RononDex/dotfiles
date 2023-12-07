74d71e496b639683570e7c3a8feb90df5cdd95b6
RononDex <tinoheuberger@protonmail.com>
2023-12-07 11:48:24 +0100

    Use newer hl api

--- defaults/.config/nvim/lua/config/debugging.lua
+++ defaults/.config/nvim/lua/config/debugging.lua
@@ -39,12 +39,17 @@ dap.configurations.cs = {
 }
 
 -- Breakpoints apperance
-vim.highlight.create('DapBreakpoint', { ctermbg = 0, guifg = '#993939', guibg = '#31353f' }, false)
-vim.highlight.create('DapLogPoint', { ctermbg = 0, guifg = '#61afef', guibg = '#31353f' }, false)
-vim.highlight.create('DapStopped', { ctermbg = 0, guifg = '#98c379', guibg = '#31353f' }, false)
+vim.api.nvim_set_hl(0, 'DapBreakpoint', { ctermbg = 0, fg = '#993939', bg = '#31353f' })
+vim.api.nvim_set_hl(0, 'DapLogPoint', { ctermbg = 0, fg = '#61afef', bg = '#31353f' })
+vim.api.nvim_set_hl(0, 'DapStopped', { ctermbg = 0, fg = '#98c379', bg = '#31353f' })
 
-vim.fn.sign_define('DapBreakpoint', { text = '', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl =
-'DapBreakpoint' })
+vim.fn.sign_define('DapBreakpoint', {
+	text = '',
+	texthl = 'DapBreakpoint',
+	linehl = 'DapBreakpoint',
+	numhl =
+	'DapBreakpoint'
+})
 vim.fn.sign_define('DapBreakpointCondition',
 	{ text = 'ﳁ', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl = 'DapBreakpoint' })
 vim.fn.sign_define('DapBreakpointRejected',
