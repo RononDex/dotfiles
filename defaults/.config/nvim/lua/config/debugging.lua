05c59a2a64f55314485defd5e36a57ce1466856c
RononDex <tinoheuberger@protonmail.com>
2023-12-07 11:05:53 +0100

    Setup a default C# DAP

--- defaults/.config/nvim/lua/config/debugging.lua
+++ defaults/.config/nvim/lua/config/debugging.lua
@@ -15,3 +15,22 @@ vim.keymap.set('n', '<F9>', '<cmd>lua require("dap").toggle_breakpoint()<cr>')
 vim.keymap.set('n', '<F5>', '<cmd>lua require("dap").continue()<cr>')
 vim.keymap.set('n', '<F10>', '<cmd>lua require("dap").step_over()<cr>')
 vim.keymap.set('n', '<F11>', '<cmd>lua require("dap").step_into()<cr>')
+vim.keymap.set('n', '<F12>', '<cmd>lua require("dap").step_out()<cr>')
+
+
+dap.adapters.coreclr = {
+	type = 'executable',
+	command = '~/.local/share/netcoredbg/netcoredbg',
+	args = { '--interpreter=vscode' }
+}
+
+dap.configurations.cs = {
+	{
+		type = "coreclr",
+		name = "launch - netcoredbg",
+		request = "launch",
+		program = function()
+			return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+		end,
+	},
+}
