dd8d219331e4e7571b12f26bdbb372c038f3a9bb
RononDex <tinoheuberger@protonmail.com>
2023-12-07 11:40:13 +0100

    Some more convience debugging keybinds

--- defaults/.config/nvim/lua/config/debugging.lua
+++ defaults/.config/nvim/lua/config/debugging.lua
@@ -13,11 +13,13 @@ dap.listeners.before.event_exited["dapui_config"] = function()
 end
 
 vim.keymap.set('n', '<F9>', '<cmd>lua require("dap").toggle_breakpoint()<cr>')
+vim.keymap.set('n', '<S-F9>', '<cmd>lua require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: ")<cr>') -- Set Breakpoint condition
 vim.keymap.set('n', '<F5>', '<cmd>lua require("dap").continue()<cr>')
+vim.keymap.set('n', '<C-F5>', '<cmd>lua require("dap").run_last()<cr>')                                            -- Run last debug again
+vim.keymap.set('n', '<S-F5>', '<cmd>lua require("dap").disconnect({ terminateDebuggee = true })<<cr>')             -- Stop debugging
 vim.keymap.set('n', '<F10>', '<cmd>lua require("dap").step_over()<cr>')
 vim.keymap.set('n', '<F11>', '<cmd>lua require("dap").step_into()<cr>')
 vim.keymap.set('n', '<F12>', '<cmd>lua require("dap").step_out()<cr>')
-
 
 dap.adapters.coreclr = {
 	type = 'executable',
