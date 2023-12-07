3424f75e19649beb8daca41f7807417d6b49b9e3
RononDex <tinoheuberger@protonmail.com>
2023-12-07 11:44:43 +0100

    Change how breakpoints look

--- defaults/.config/nvim/lua/config/debugging.lua
+++ defaults/.config/nvim/lua/config/debugging.lua
@@ -37,3 +37,17 @@ dap.configurations.cs = {
 		end,
 	},
 }
+
+-- Breakpoints apperance
+vim.highlight.create('DapBreakpoint', { ctermbg = 0, guifg = '#993939', guibg = '#31353f' }, false)
+vim.highlight.create('DapLogPoint', { ctermbg = 0, guifg = '#61afef', guibg = '#31353f' }, false)
+vim.highlight.create('DapStopped', { ctermbg = 0, guifg = '#98c379', guibg = '#31353f' }, false)
+
+vim.fn.sign_define('DapBreakpoint', { text = '', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl =
+'DapBreakpoint' })
+vim.fn.sign_define('DapBreakpointCondition',
+	{ text = 'ﳁ', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl = 'DapBreakpoint' })
+vim.fn.sign_define('DapBreakpointRejected',
+	{ text = '', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl = 'DapBreakpoint' })
+vim.fn.sign_define('DapLogPoint', { text = '', texthl = 'DapLogPoint', linehl = 'DapLogPoint', numhl = 'DapLogPoint' })
+vim.fn.sign_define('DapStopped', { text = '', texthl = 'DapStopped', linehl = 'DapStopped', numhl = 'DapStopped' })
