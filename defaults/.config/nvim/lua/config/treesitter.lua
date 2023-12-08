74290413393f305ef6c96e3445df55776183c7e2
RononDex <tinoheuberger@protonmail.com>
2023-12-08 15:03:39 +0100

    Propery setup treesiiter

--- defaults/.config/nvim/lua/config/treesitter.lua
+++ defaults/.config/nvim/lua/config/treesitter.lua
@@ -1,4 +1,8 @@
 require('nvim-treesitter.configs').setup({
 	-- A list of parser names, or "all" (the five listed parsers should always be installed)
 	ensure_installed = { "c", "c_sharp", "lua", "vim", "vimdoc", "query", "java", "cpp", "json", "xml", "yaml" },
+	auto_install = true,
+	highlight = {
+		enable = true,
+	}
 })
