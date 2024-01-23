f5450f35971c8cd6980787da8ba2f1b7733b7985
RononDex <tinoheuberger@protonmail.com>
2024-01-23 10:58:55 +0100

    Attempt at disabling wins in tabs for tabby

--- defaults/.config/nvim/lua/config/tabby.lua
+++ defaults/.config/nvim/lua/config/tabby.lua
@@ -27,16 +27,17 @@ require('tabby.tabline').set(function(line)
 			}
 		end),
 		line.spacer(),
-		line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
-			return {
-				line.sep('', theme.win, theme.fill),
-				win.is_current() and '' or '',
-				win.buf_name(),
-				line.sep('', theme.win, theme.fill),
-				hl = theme.win,
-				margin = ' ',
-			}
-		end),
+		"",
+		-- line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
+		-- 	return {
+		-- 		line.sep('', theme.win, theme.fill),
+		-- 		win.is_current() and '' or '',
+		-- 		win.buf_name(),
+		-- 		line.sep('', theme.win, theme.fill),
+		-- 		hl = theme.win,
+		-- 		margin = ' ',
+		-- 	}
+		-- end),
 		{
 			line.sep('', theme.tail, theme.fill),
 			{ '  ', hl = theme.tail },
