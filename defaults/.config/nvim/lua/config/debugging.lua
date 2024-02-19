ae941ca5af96d37e7c1ce90f061f84caec5bc9e0
RononDex <tinoheuberger@protonmail.com>
2024-02-19 10:34:07 +0100

    Add missin program entry

--- defaults/.config/nvim/lua/config/debugging.lua
+++ defaults/.config/nvim/lua/config/debugging.lua
@@ -34,6 +34,9 @@ dap.configurations.cs = {
 		type = "coreclr",
 		name = "launch - netcoredbg",
 		request = "launch",
+		program = function()
+			return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+		end,
 	}
 }
 
