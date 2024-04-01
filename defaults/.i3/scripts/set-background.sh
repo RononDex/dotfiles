5258076f095583918078fc33b88f628cd6850786
RononDex <tinoheuberger@protonmail.com>
2024-04-01 18:18:35 +0200

    Setup GTK theming under wayland by importing gtk settings.ini

--- defaults/.i3/scripts/set-background.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wallpapersDir="${HOME}/wallpapers/"
-backgroundImage=$(find ${wallpapersDir} -type f | shuf -n 1) # select random backgroundImage
-echo $backgroundImage
-wal -i $backgroundImage --saturate 0.75
