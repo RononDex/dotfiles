a8607180c83f985b95a39ac3630f089f4ca0d0ff
RononDex <tinoheuberger@protonmail.com>
2024-04-13 10:35:52 +0200

    Keybind for neotest summary

--- defaults/.config/nvim/lua/config/neotest.lua
+++ defaults/.config/nvim/lua/config/neotest.lua
@@ -13,3 +13,4 @@ require("neotest").setup({
 vim.keymap.set('n', '<leader>td', "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>", {})
 vim.keymap.set('n', '<leader>tr', "<cmd>lua require('neotest').run.run()<cr>", {})
 vim.keymap.set('n', '<leader>tf', "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>", {})
+vim.keymap.set('n', '<leader>ts', "<cmd>Neotest summary<cr>", {})
