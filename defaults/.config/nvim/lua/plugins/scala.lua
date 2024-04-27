9b9170815e9f525f7c9ddf2aac59d0e34efbb853
RononDex <tinoheuberger@protonmail.com>
2024-04-27 10:16:49 +0200

    Setup scala dap

--- defaults/.config/nvim/lua/plugins/scala.lua
+++ defaults/.config/nvim/lua/plugins/scala.lua
@@ -27,5 +27,27 @@ return {
 			end,
 			group = nvim_metals_group,
 		})
+
+		local dap = require("dap")
+
+		dap.configurations.scala = {
+			{
+				type = "scala",
+				request = "launch",
+				name = "RunOrTest",
+				metals = {
+					runType = "runOrTestFile",
+					--args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
+				},
+			},
+			{
+				type = "scala",
+				request = "launch",
+				name = "Test Target",
+				metals = {
+					runType = "testTarget",
+				},
+			},
+		}
 	end
 }
