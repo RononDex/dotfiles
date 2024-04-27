9c0e5155ed027d37d326ac16532cd42d12dab9ff
RononDex <tinoheuberger@protonmail.com>
2024-04-27 14:58:26 +0200

    Remove redundant code

--- defaults/.config/nvim/lua/plugins/scala.lua
+++ defaults/.config/nvim/lua/plugins/scala.lua
@@ -22,20 +22,6 @@ return {
 		return metals_config
 	end,
 	config = function(self, metals_config)
-		local metals_config = require("metals").bare_config()
-		metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
-		-- Example of settings
-		metals_config.settings = {
-			showImplicitArguments = true,
-			excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
-			superMethodLensesEnabled = true,
-			showInferredType = true,
-			showImplicitConversionsAndClasses = true,
-		}
-		metals_config.on_attach = function(client, bufnr)
-			require("metals").setup_dap()
-		end
-
 		local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
 		vim.api.nvim_create_autocmd("FileType", {
 			pattern = self.ft,
