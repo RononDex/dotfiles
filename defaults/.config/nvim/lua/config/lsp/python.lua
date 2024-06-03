7f90f93eb2890774bae4937bb2977a3e4fb17f6a
RononDex <tinoheuberger@protonmail.com>
2024-06-03 15:53:44 +0200

    Update to new branch for venv-select

--- defaults/.config/nvim/lua/config/lsp/python.lua
+++ defaults/.config/nvim/lua/config/lsp/python.lua
@@ -1,3 +1,4 @@
+require("venv-selector").setup()
 vim.api.nvim_create_autocmd('VimEnter', {
 	desc = 'Auto select virtualenv Nvim open',
 	pattern = '*',
--- defaults/.config/nvim/lua/plugins/python.lua
+++ defaults/.config/nvim/lua/plugins/python.lua
@@ -13,6 +13,7 @@ return {
 				},
 			})
 		end,
+		branch = "regexp", -- This is the regexp branch, use this for the new version
 		keys = {
 			-- Keymap to open VenvSelector to pick a venv.
 			{ '<leader>vs', '<cmd>VenvSelect<cr>' },
