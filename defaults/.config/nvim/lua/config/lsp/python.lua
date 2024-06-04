8e0d653b60c7cacf48043e7e55b9e9de9f5910fc
RononDex <tinoheuberger@protonmail.com>
2024-06-04 16:03:01 +0200

    Now no longer needed after venv update

--- defaults/.config/nvim/lua/config/lsp/python.lua
+++ defaults/.config/nvim/lua/config/lsp/python.lua
@@ -1,9 +1,1 @@
 require("venv-selector").setup()
-vim.api.nvim_create_autocmd('VimEnter', {
-	desc = 'Auto select virtualenv Nvim open',
-	pattern = '*',
-	callback = function()
-		require('venv-selector').retrieve_from_cache()
-	end,
-	once = true,
-})
