8bd0908f88858bc703d4082b73bcaa55929ee552
RononDex <tinoheuberger@protonmail.com>
2024-07-08 14:03:03 +0200

    Install missing gpg key

--- default-profile-install-arch.sh
+++ default-profile-install-arch.sh
@@ -135,6 +135,9 @@ InstallAurPackage "dcron" "https://aur.archlinux.org/dcron.git"
 InstallAurPackage "nomacs" "https://aur.archlinux.org/nomacs.git"
 InstallAurPackage "ddcui" "https://aur.archlinux.org/ddcui.git"
 InstallAurPackage "svp-bin" "https://aur.archlinux.org/svp-bin.git"
+
+# Needed for librewolf
+gpg --keyserver keyserver.ubuntu.com --recv-keys 662E3CDD6FE329002D0CA5BB40339DD82B12EF16
 InstallAurPackage "librewolf-bin" "https://aur.archlinux.org/librewolf-bin.git"
 
 echo "Setting up Display Manager"
