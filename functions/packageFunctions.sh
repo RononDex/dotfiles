b909c504365deabe35d269dda52d316502e19cc7
RononDex <tinoheuberger@protonmail.com>
2024-10-01 13:33:26 +0200

    Use official upstream versions instead of git

--- functions/packageFunctions.sh
+++ functions/packageFunctions.sh
@@ -83,12 +83,12 @@ InstallHyprland() {
     then
 		gpg --receive-keys 0FDE7BE0E88F5E48 # Adds needed key for AUR packages
 
-		yes | sudo pacman -S hyprland nwg-look xdg-desktop-portal-hyprland hyprutils libdisplay-info waybar --needed --noconfirm
-		InstallAurPackage "wlr-randr" "https://aur.archlinux.org/wlr-randr.git"
+		yes | sudo pacman -S hyprland nwg-look xdg-desktop-portal-hyprland hyprutils wlr-randr hyprlock hypridle libdisplay-info waybar --needed --noconfirm
+		# InstallAurPackage "wlr-randr" "https://aur.archlinux.org/wlr-randr.git"
 		# InstallAurPackage "hyprutils-git" "https://aur.archlinux.org/hyprutils-git.git"
 		# InstallAurPackage "hyprland-git" "https://aur.archlinux.org/hyprland-git.git" "-f"
-		InstallAurPackage "hyprlock-git" "https://aur.archlinux.org/hyprlock-git.git"
-		InstallAurPackage "hypridle-git" "https://aur.archlinux.org/hypridle-git.git"
+		# InstallAurPackage "hyprlock-git" "https://aur.archlinux.org/hyprlock-git.git"
+		# InstallAurPackage "hypridle-git" "https://aur.archlinux.org/hypridle-git.git"
 		InstallAurPackage "nwg-displays" "https://aur.archlinux.org/nwg-displays.git"
 
 		sudo pacman -Sy wofi swaybg --needed --noconfirm
