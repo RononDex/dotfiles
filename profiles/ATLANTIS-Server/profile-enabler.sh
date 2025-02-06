4980a244bf218f077d0851858d86d7cebd8fed9c
Tino Heuberger <tino.heuberger@fhnw.ch>
2025-02-06 09:49:38 +0100

    Install postfix for sendmail binary

--- profiles/ATLANTIS-Server/profile-enabler.sh
+++ profiles/ATLANTIS-Server/profile-enabler.sh
@@ -48,7 +48,7 @@ sudo xbps-install -y qemu virt-manager smartmontools
 sudo xbps-install -y polybar python3-vdf protontricks vscode ckb-next screenkey vscode gnuplot
 sudo xbps-install -y xf86-video-amdgpu vulkan-loader mesa cryptsetup tpm2-tss cronie mesa-vaapi mesa-vdpau mesa-vulkan-radeon mesa-dri
 sudo xbps-install -y nomacs breeze breeze-cursors samba
-sudo xbps-install -y mdadm wireguard tigervnc docker
+sudo xbps-install -y mdadm wireguard tigervnc docker postfix
 
 InstallXorg
 InstallYubiKeyStuff
