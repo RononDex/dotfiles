af8c104704fe15826544a10028d986f556fe6ed7
Tino Heuberger <tinoheuberger@protonmail.com>
2025-03-27 13:38:06 +0100

    Reference latest LTS release

--- defaults/.config/nvim/lua/config/lsp/jdtls.lua
+++ defaults/.config/nvim/lua/config/lsp/jdtls.lua
@@ -93,8 +93,8 @@ local function get_jdtls_paths()
 			path = "/usr/lib/jvm/java-17-openjdk",
 		},
 		{
-			name = "JavaSE-24",
-			path = "/usr/lib/jvm/java-24-openjdk",
+			name = "JavaSE-21",
+			path = "/usr/lib/jvm/java-21-openjdk",
 			default = true
 		}
 	}
