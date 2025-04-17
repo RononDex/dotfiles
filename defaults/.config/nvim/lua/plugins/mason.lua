81d4af919f12cd859232bb5bc9224c4c571694aa
Tino Heuberger <tinoheuberger@protonmail.com>
2025-04-17 16:46:31 +0200

    Reenable nvim-lspconfig for LspInfo command

--- defaults/.config/nvim/lua/plugins/mason.lua
+++ defaults/.config/nvim/lua/plugins/mason.lua
@@ -2,7 +2,7 @@ return {
 	{ 'williamboman/mason.nvim' },
 	-- { 'williamboman/mason-lspconfig.nvim' },
 	-- { 'VonHeikemen/lsp-zero.nvim' },
-	-- { 'neovim/nvim-lspconfig' },
+	{ 'neovim/nvim-lspconfig' },
 	{ 'hrsh7th/cmp-nvim-lsp' },
 	{ 'hrsh7th/nvim-cmp' },
 	{ 'L3MON4D3/LuaSnip' },
