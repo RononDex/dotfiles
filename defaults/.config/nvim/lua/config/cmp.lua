817f923501646d9868496bfe46dc2fca918d319a
Tino Heuberger <tinoheuberger@protonmail.com>
2025-04-17 10:53:39 +0200

    Remove call to non-existant method

--- defaults/.config/nvim/lua/config/cmp.lua
+++ defaults/.config/nvim/lua/config/cmp.lua
@@ -19,8 +19,6 @@ cmp.setup({
 				-- they way you will only jump inside the snippet region
 			elseif luasnip.expand_or_jumpable() then
 				luasnip.expand_or_jump()
-			elseif has_words_before() then
-				cmp.complete()
 			else
 				fallback()
 			end
