8763391adb0bd962abb66de176d015289193c48d
Tino Heuberger <tinoheuberger@protonmail.com>
2025-04-17 08:51:46 +0200

    Attempt to fix lua ls

--- defaults/.config/nvim/lua/lsp/lua.lua
+++ defaults/.config/nvim/lua/lsp/lua.lua
@@ -2,6 +2,12 @@ local root_files = {
 	'.git',
 }
 
+local luals_install = require('mason-registry')
+		.get_package('lua-language-server')
+		:get_install_path()
+
+print(luals_install)
+
 vim.lsp.config('lua', {
 		name = "lua",
 		cmd = { "luals" },
