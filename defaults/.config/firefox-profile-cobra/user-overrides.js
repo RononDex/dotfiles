402af80c8c0318ab1af175cc5aebf46840ba4ab7
Tino Heuberger <tino.heuberger@fhnw.ch>
2025-05-06 14:57:43 +0200

    Keep cookies after shutdown (isoloated using plugins)

--- defaults/.config/firefox-profile-cobra/user-overrides.js
+++ defaults/.config/firefox-profile-cobra/user-overrides.js
@@ -2,6 +2,7 @@
 user_pref("browser.startup.page", 3); // 0102
   // user_pref("browser.privatebrowsing.autostart", false); // 0110 required if you had it set as true
 user_pref("browser.sessionstore.privacy_level", 0); // 1003 optional to restore cookies/formdata
+user_pref("privacy.clearOnShutdown_v2.cookiesAndStorage", false);
 user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", false); // 2811 FF128-135
 user_pref("privacy.clearOnShutdown_v2.browsingHistoryAndDownloads", false); // 2812 FF136+
 
