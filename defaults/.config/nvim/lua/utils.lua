bc35dfb3a8a618226c1353b6d8bfb7698a3da1fc
Tino Heuberger <tinoheuberger@protonmail.com>
2025-05-07 11:00:46 +0200

    Another attempted fix

--- defaults/.config/nvim/lua/utils.lua
+++ defaults/.config/nvim/lua/utils.lua
@@ -48,7 +48,7 @@ end
 function utils.get_mason_dir(package_name)
 	local mason_registry = require("mason-registry")
 	local package = mason_registry.get_package(package_name)
-	if package not nil then
+	if package ~= nil then
 			return package:get_install_path()
 	else
 			return "python"
