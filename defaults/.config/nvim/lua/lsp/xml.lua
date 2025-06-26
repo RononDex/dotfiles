6ba1d8fae610942e6ffc52aec44a74076f586771
Tino Heuberger <tinoheuberger@protonmail.com>
2025-06-26 13:12:15 +0200

    Enable single file for lemminx

--- defaults/.config/nvim/lua/lsp/xml.lua
+++ defaults/.config/nvim/lua/lsp/xml.lua
@@ -2,9 +2,11 @@ vim.lsp.config('xml', {
 	cmd = { "lemminx" },
 	filetypes = { 'xml', 'xsd', 'xsl', 'xslt', 'svg' },
 	root_markers = { '.git' },
+	single_file_support = true,
 	settings = {
 		xml = {
 			format = {
+				enabled = true,
 				joinContentLines = false
 			}
 		}
