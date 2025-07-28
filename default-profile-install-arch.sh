8e10e9a6ccecbfdc280bc7651f322bbd2430772a
Tino Heuberger <tinoheuberger@protonmail.com>
2025-07-29 00:09:42 +0200

    Remove xorg related stuff from default install

--- default-profile-install-arch.sh
+++ default-profile-install-arch.sh
@@ -64,16 +64,16 @@ source ~/.profile
 
 echo "Installing stuff..."
 sudo pacman -S pkgconfig bc powerline-fonts fakeroot debugedit ttf-firacode-nerd ttf-arimo-nerd gcc boost ffmpeg make cmake otf-fira-mono otf-fira-sans ttf-fira-code ttf-fira-mono ttf-fira-sans zsh zsh-completions automake m4 autoconf --noconfirm --needed
-sudo pacman -S bash-completion networkmanager gnome-keyring network-manager-applet xorg xorg-xinit firefox adobe-source-code-pro-fonts neofetch xclip --noconfirm --needed
+sudo pacman -S bash-completion networkmanager gnome-keyring network-manager-applet firefox adobe-source-code-pro-fonts neofetch xclip --noconfirm --needed
 sudo pacman -S picom pipewire pipewire-pulse wireplumber pavucontrol arc-gtk-theme arc-icon-theme nautilus --noconfirm --needed
 sudo pacman -S java-runtime-common jre-openjdk xdotool --noconfirm --needed
 sudo pacman -S vim neovim libftdi ccfits network-manager-applet adobe-source-code-pro-fonts noto-fonts-extra --noconfirm --needed
-sudo pacman -S python debugedit acpi python-pip samba opencv pkgconfig gtest gmock libmpdclient bc ranger xorg-server binutils keychain --needed --noconfirm
+sudo pacman -S python acpi python-pip samba opencv gtest gmock libmpdclient ranger binutils keychain --needed --noconfirm
 sudo pacman -S htop pv imagemagick zlib curl exfat-utils unzip zip shadow perl-json-xs git-lfs python-pywal fzf arandr pass --needed --noconfirm
 sudo pacman -S zsh-syntax-highlighting xfce4-power-manager openvpn zsh-autosuggestions calc diff-so-fancy networkmanager-openvpn powerline-fonts zathura zathura-cb zathura-pdf-mupdf zathura-ps lynx ttf-dejavu --needed --noconfirm
 sudo pacman -S dkms btop gnupg pcsclite ccid yubikey-manager yubikey-personalization --needed --noconfirm
 sudo pacman -S ueberzug autoconf keyutils automake bison openconnect ksshaskpass --needed --noconfirm
-sudo pacman -S ttf-liberation kitty libvips lftp python-pip npm linux-firmware-marvell gtk2 ranger fwupd --needed --noconfirm
+sudo pacman -S ttf-liberation kitty libvips lftp npm linux-firmware-marvell gtk2 fwupd --needed --noconfirm
 sudo pacman -S tracker3 webkit2gtk tracker3-miners qt5ct qt6ct otf-font-awesome ddcutil jq polkit --needed --noconfirm
 sudo pacman -S gtk-engine-murrine sassc luarocks shfmt lm_sensors --needed --noconfirm
 
