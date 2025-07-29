be42cba0f59ba22eda34aa1f96a49545b786a126
Tino Heuberger <tinoheuberger@protonmail.com>
2025-07-29 23:35:23 +0200

    Reenable plugin with basic upstream config

--- defaults/.config/nvim/lua/plugins/python.lua
+++ defaults/.config/nvim/lua/plugins/python.lua
@@ -1,24 +1,21 @@
 return {
-	-- {
-	-- 	"linux-cultist/venv-selector.nvim",
-	-- 	dependencies = { 'mfussenegger/nvim-dap-python' },
-	-- 	cmd = "VenvSelect",
-	-- 	opts = function(_, opts)
-	-- 		return vim.tbl_deep_extend("force", opts, {
-	-- 			name = {
-	-- 				"venv",
-	-- 				".venv",
-	-- 				"env",
-	-- 				".env",
-	-- 			},
-	-- 		})
-	-- 	end,
-	-- 	branch = "regexp", -- This is the regexp branch, use this for the new version
-	-- 	keys = {
-	-- 		-- Keymap to open VenvSelector to pick a venv.
-	-- 		{ '<leader>vs', '<cmd>VenvSelect<cr>' },
-	-- 		-- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
-	-- 		{ '<leader>vc', '<cmd>VenvSelectCached<cr>' },
-	-- 	},
-	-- },
+		{
+  "linux-cultist/venv-selector.nvim",
+  dependencies = {
+    "neovim/nvim-lspconfig",
+    "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", --optional
+    { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+  },
+  lazy = false,
+  branch = "regexp", -- This is the regexp branch, use this for the new version
+  keys = {
+	-- Keymap to open VenvSelector to pick a venv.
+	{ '<leader>vs', '<cmd>VenvSelect<cr>' },
+	-- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+	{ '<leader>vc', '<cmd>VenvSelectCached<cr>' },
+  },
+  ---@type venv-selector.Config
+  opts = {
+  }
+}
 }
