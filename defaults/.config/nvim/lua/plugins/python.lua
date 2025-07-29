5d8d372f152f72c0c5c442c2b7de0b4c78d55ff5
Tino Heuberger <tinoheuberger@protonmail.com>
2025-07-29 23:28:20 +0200

    Remove broken venv plugin for now

--- defaults/.config/nvim/lua/plugins/python.lua
+++ defaults/.config/nvim/lua/plugins/python.lua
@@ -1,24 +1,24 @@
 return {
-	{
-		"linux-cultist/venv-selector.nvim",
-		dependencies = { 'mfussenegger/nvim-dap-python' },
-		cmd = "VenvSelect",
-		opts = function(_, opts)
-			return vim.tbl_deep_extend("force", opts, {
-				name = {
-					"venv",
-					".venv",
-					"env",
-					".env",
-				},
-			})
-		end,
-		branch = "regexp", -- This is the regexp branch, use this for the new version
-		keys = {
-			-- Keymap to open VenvSelector to pick a venv.
-			{ '<leader>vs', '<cmd>VenvSelect<cr>' },
-			-- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
-			{ '<leader>vc', '<cmd>VenvSelectCached<cr>' },
-		},
-	},
+	-- {
+	-- 	"linux-cultist/venv-selector.nvim",
+	-- 	dependencies = { 'mfussenegger/nvim-dap-python' },
+	-- 	cmd = "VenvSelect",
+	-- 	opts = function(_, opts)
+	-- 		return vim.tbl_deep_extend("force", opts, {
+	-- 			name = {
+	-- 				"venv",
+	-- 				".venv",
+	-- 				"env",
+	-- 				".env",
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- 	branch = "regexp", -- This is the regexp branch, use this for the new version
+	-- 	keys = {
+	-- 		-- Keymap to open VenvSelector to pick a venv.
+	-- 		{ '<leader>vs', '<cmd>VenvSelect<cr>' },
+	-- 		-- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+	-- 		{ '<leader>vc', '<cmd>VenvSelectCached<cr>' },
+	-- 	},
+	-- },
 }
