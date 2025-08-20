e8638888e8b8bf6ff402ca893b4378aa16b4ca70
Tino Heuberger <tinoheuberger@protonmail.com>
2025-08-20 08:58:18 +0200

    Preserve login session on firefox restart

--- defaults/.config/firefox-profile-cobra/user-overrides.js
+++ defaults/.config/firefox-profile-cobra/user-overrides.js
@@ -1,15 +1,24 @@
 /* override recipe: enable session restore ***/
 user_pref("browser.startup.page", 3); // 0102
-  // user_pref("browser.privatebrowsing.autostart", false); // 0110 required if you had it set as true
+user_pref("browser.privatebrowsing.autostart", false); // 0110 required if you had it set as true
 user_pref("browser.sessionstore.privacy_level", 0); // 1003 optional to restore cookies/formdata
+user_pref("places.history.enabled", true);                  // 0862 – re-enable history
+user_pref("network.cookie.lifetimePolicy", 0);              // 2801 – persist cookies
 user_pref("privacy.clearOnShutdown_v2.cookiesAndStorage", false);
 user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", false); // 2811 FF128-135
 user_pref("privacy.clearOnShutdown_v2.browsingHistoryAndDownloads", false); // 2812 FF136+
+user_pref("privacy.clearSiteData.browsingHistoryAndDownloads", false); //2821
+user_pref("privacy.clearHistory.browsingHistoryAndDownloads", false); // 2831
+user_pref("privacy.clearOnShutdown.cookies", false);        // 2811 – don't clear cookies on shutdown
+user_pref("privacy.clearOnShutdown.sessions", false);       // 2811 – don't clear active logins
+user_pref("privacy.cpd.cookies", false);                    // 2812 – don't clear cookies manually by default
+user_pref("privacy.cpd.sessions", false);                   // 2812 – don't clear active logins manually by default
+user_pref("signon.rememberSignons", false);                  // 5003 – don't allow Firefox to save logins/passwords
 
 // optional to match when you use settings>Cookies and Site Data>Clear Data
-  // user_pref("privacy.clearSiteData.historyFormDataAndDownloads", false); // 2820 FF128-135
-  // user_pref("privacy.clearSiteData.browsingHistoryAndDownloads", false); // 2821 FF136+
+// user_pref("privacy.clearSiteData.historyFormDataAndDownloads", false); // 2820 FF128-135
+// user_pref("privacy.clearSiteData.browsingHistoryAndDownloads", false); // 2821 FF136+
 
 // optional to match when you use Ctrl-Shift-Del (settings>History>Custom Settings>Clear History)
-  // user_pref("privacy.clearHistory.historyFormDataAndDownloads", false); // 2830 FF128-135
-  // user_pref("privacy.clearHistory.browsingHistoryAndDownloads", false); // 2831 FF136+
+// user_pref("privacy.clearHistory.historyFormDataAndDownloads", false); // 2830 FF128-135
+// user_pref("privacy.clearHistory.browsingHistoryAndDownloads", false); // 2831 FF136+
