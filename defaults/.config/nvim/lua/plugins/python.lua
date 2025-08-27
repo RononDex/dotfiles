3162ca87462ad0a7f7161e6bcc2128825b075e0d
Tino Heuberger <tinoheuberger@protonmail.com>
2025-08-27 23:13:49 +0200

    New version is now on main branch

--- defaults/.config/nvim/lua/plugins/python.lua
+++ defaults/.config/nvim/lua/plugins/python.lua
@@ -1,20 +1,20 @@
 return {
-		{
-  "linux-cultist/venv-selector.nvim",
-  dependencies = {
-    "neovim/nvim-lspconfig",
-    "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", --optional
-  },
-  lazy = false,
-  branch = "regexp", -- This is the regexp branch, use this for the new version
-  keys = {
-	-- Keymap to open VenvSelector to pick a venv.
-	{ '<leader>vs', '<cmd>VenvSelect<cr>' },
-	-- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
-	{ '<leader>vc', '<cmd>VenvSelectCached<cr>' },
-  },
-  ---@type venv-selector.Config
-  opts = {
-  }
-}
+	{
+		"linux-cultist/venv-selector.nvim",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", --optional
+		},
+		lazy = false,
+		branch = "main", -- This is the regexp branch, use this for the new version
+		keys = {
+			-- Keymap to open VenvSelector to pick a venv.
+			{ '<leader>vs', '<cmd>VenvSelect<cr>' },
+			-- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+			{ '<leader>vc', '<cmd>VenvSelectCached<cr>' },
+		},
+		---@type venv-selector.Config
+		opts = {
+		}
+	}
 }
