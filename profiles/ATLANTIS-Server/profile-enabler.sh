d12b6dbb93cda12282c333c2ea047072a63beb86
Tino Heuberger <tinoheuberger@protonmail.com>
2025-09-02 21:03:59 +0200

    Use restic for backups from now on

--- profiles/ATLANTIS-Server/profile-enabler.sh
+++ profiles/ATLANTIS-Server/profile-enabler.sh
@@ -32,7 +32,7 @@ sudo cp $scriptDir/overrides/samba/smb.conf /etc/samba/smb.conf
 sudo mkdir -p /data
 sudo cp ~/.scripts/utilities/updateAndReboot /root/updateAndReboot
 sudo cp $scriptDir/overrides/scripts/doBackup /root/doBackup
-sudo cp ~/.scripts/backup/backupServerToKDrive /root/backupServerToKDrive
+sudo cp ~/.scripts/backup/backupResticServer /root/backupResticServer
 
 sudo cp $scriptDir/overrides/sysctl.conf /etc/sysctl.conf
 sudo sysctl -p
