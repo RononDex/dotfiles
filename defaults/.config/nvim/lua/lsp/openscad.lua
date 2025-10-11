a127eba8ea4991c23511efabf96a233f277737de
Tino Heuberger <tinoheuberger@protonmail.com>
2025-10-11 15:39:14 +0200

    Do not decclare root files

--- defaults/.config/nvim/lua/lsp/openscad.lua
+++ defaults/.config/nvim/lua/lsp/openscad.lua
@@ -6,7 +6,7 @@ vim.lsp.config('openscad', {
 	name = "openscad_lsp",
 	cmd = { "openscad-lsp" },
 	filetypes = { "openscad" },
-	root_markers = root_files,
+	-- root_markers = root_files,
 	settings = {
 	},
 	single_file_support = true
