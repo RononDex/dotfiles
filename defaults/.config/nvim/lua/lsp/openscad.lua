b1194689ce8a40d767e76403824da819e3c65786
Tino Heuberger <tinoheuberger@protonmail.com>
2025-10-13 22:46:46 +0200

    Use stdio for openscad

--- defaults/.config/nvim/lua/lsp/openscad.lua
+++ defaults/.config/nvim/lua/lsp/openscad.lua
@@ -4,9 +4,11 @@ local root_files = {
 
 vim.lsp.config('openscad', {
 	name = "openscad_lsp",
-	cmd = { "openscad-lsp" },
+	cmd = { "openscad-lsp", "--stdio" },
 	filetypes = { "openscad" },
-	-- root_markers = root_files,
+	root_dir = function(fname)
+		return vim.fs.dirname(vim.fs.find('.git', { path = fname, upward = true })[1])
+	end,
 	settings = {
 	},
 	single_file_support = true
