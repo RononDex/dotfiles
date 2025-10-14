c3c29e6c92320743c28e1d2ccac6addabafb780f
Tino Heuberger <tinoheuberger@protonmail.com>
2025-10-14 10:17:03 +0200

    Use a function for root_dir

--- defaults/.config/nvim/lua/lsp/openscad.lua
+++ defaults/.config/nvim/lua/lsp/openscad.lua
@@ -1,12 +1,8 @@
-local root_files = {
-	'.git',
-}
-
 vim.lsp.config('openscad', {
 	name = "openscad_lsp",
 	cmd = { 'openscad-lsp', '--stdio' },
 	filetypes = { 'openscad' },
-	root_dir = root_files,
+	root_dir = vim.fs.dirname(vim.fs.find({ ".git" }, { upward = true })[1]),
 	settings = {
 	},
 	single_file_support = true
