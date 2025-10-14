6e710be6e4c73006763afcf62ace91ceecebda29
Tino Heuberger <tinoheuberger@protonmail.com>
2025-10-14 13:30:00 +0200

    Keybind to render in openscad

--- defaults/.config/nvim/lua/lsp/openscad.lua
+++ defaults/.config/nvim/lua/lsp/openscad.lua
@@ -7,3 +7,32 @@ vim.lsp.config('openscad', {
 	},
 	single_file_support = true
 });
+
+vim.api.nvim_create_autocmd('LspAttach', {
+	group = vim.api.nvim_create_augroup('openscad.lsp', {}),
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if not client then
+			return
+		end
+
+		if client.name == "openscad_lsp" then
+			vim.keymap.set("n", "<F5>", exec_openscad)
+		end
+	end,
+})
+
+function exec_openscad()
+	local jobCommand;
+	local filename = '"' .. vim.fn.expand("%:p") .. '"'
+
+	-- If Linux, just use basecommand, if on MacOS, use a special command
+	if vim.fn.has('mac') == 1 then
+		jobCommand = '/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD ' .. filename
+	else
+		-- TODO: What about Windows?
+		jobCommand = 'openscad ' .. filename
+	end
+
+	vim.fn.jobstart(jobCommand)
+end
