76106b0912ff04aa94cd2b11e672feae864b541b
Tino Heuberger <tino.heuberger@fhnw.ch>
2025-12-15 10:21:25 +0100

    Switch tresitter to main branch

--- defaults/.config/nvim/lua/plugins/treesitter.lua
+++ defaults/.config/nvim/lua/plugins/treesitter.lua
@@ -1,31 +1,32 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  dependencies = { "windwp/nvim-ts-autotag", "JoosepAlviste/nvim-ts-context-commentstring" },
-  event = "User AstroFile",
-  cmd = {
-    "TSBufDisable",
-    "TSBufEnable",
-    "TSBufToggle",
-    "TSDisable",
-    "TSEnable",
-    "TSToggle",
-    "TSInstall",
-    "TSInstallInfo",
-    "TSInstallSync",
-    "TSModuleInfo",
-    "TSUninstall",
-    "TSUpdate",
-    "TSUpdateSync",
-  },
-  build = ":TSUpdate",
-  opts = {
-    highlight = { enable = true },
-    incremental_selection = { enable = true },
-    indent = { enable = true },
-    autotag = { enable = true },
-    context_commentstring = { enable = true, enable_autocmd = false },
-    install = { -- not working
-      compilers = { "clang", "gcc" },  -- not working
-    },  -- not working
-  },
+	"nvim-treesitter/nvim-treesitter",
+	dependencies = { "windwp/nvim-ts-autotag", "JoosepAlviste/nvim-ts-context-commentstring" },
+	event = "User AstroFile",
+	branch = "main"
+	cmd = {
+		"TSBufDisable",
+		"TSBufEnable",
+		"TSBufToggle",
+		"TSDisable",
+		"TSEnable",
+		"TSToggle",
+		"TSInstall",
+		"TSInstallInfo",
+		"TSInstallSync",
+		"TSModuleInfo",
+		"TSUninstall",
+		"TSUpdate",
+		"TSUpdateSync",
+	},
+	build = ":TSUpdate",
+	opts = {
+		highlight = { enable = true },
+		incremental_selection = { enable = true },
+		indent = { enable = true },
+		autotag = { enable = true },
+		context_commentstring = { enable = true, enable_autocmd = false },
+		install = {           -- not working
+			compilers = { "clang", "gcc" }, -- not working
+		},                    -- not working
+	},
 }
