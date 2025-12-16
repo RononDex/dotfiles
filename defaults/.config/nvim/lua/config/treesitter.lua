eef0f70e73b748f13019d3de91b2be072ffbc64c
Tino Heuberger <tinoheuberger@protonmail.com>
2025-12-16 09:06:07 +0100

    Add some treesitter parsers by default

--- defaults/.config/nvim/lua/config/treesitter.lua
+++ defaults/.config/nvim/lua/config/treesitter.lua
@@ -1,2 +1,4 @@
 require('nvim-treesitter').install({ "c", "c_sharp", "lua", "vim", "vimdoc", "query", "java", "cpp", "json", "xml",
+	"bash", "html", "javascript", "markdown", "make", "sql", "hyprlang", "javadoc", "python",
 	"yaml", "rust" })
+require('nvim-treesitter').update()
