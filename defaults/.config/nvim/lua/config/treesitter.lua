b33d918532fa8673ef480702e143b04bf50a8681
Tino Heuberger <tinoheuberger@protonmail.com>
2025-12-17 09:49:02 +0100

    Disable indentation by treesitter

--- defaults/.config/nvim/lua/config/treesitter.lua
+++ defaults/.config/nvim/lua/config/treesitter.lua
@@ -14,7 +14,7 @@ vim.api.nvim_create_autocmd('FileType', {
 		vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 		vim.wo.foldmethod = 'expr'
 		-- indentation, provided by nvim-treesitter
-		vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+		-- vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 		vim.wo.foldlevel = 99;
 	end,
 })
