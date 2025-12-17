9c759c98549de473db5767efc657c8575a36e994
Tino Heuberger <tinoheuberger@protonmail.com>
2025-12-17 09:17:01 +0100

    Setup autocmd for filetypes and treesitter

--- defaults/.config/nvim/lua/config/treesitter.lua
+++ defaults/.config/nvim/lua/config/treesitter.lua
@@ -1,4 +1,19 @@
-require('nvim-treesitter').install({ "c", "c_sharp", "lua", "vim", "vimdoc", "query", "java", "cpp", "json", "xml",
+local language_list = { "c", "c_sharp", "lua", "vim", "vimdoc", "query", "java", "cpp", "json", "xml",
 	"bash", "html", "javascript", "markdown", "make", "sql", "hyprlang", "javadoc", "python",
-	"yaml", "rust" })
+	"yaml", "rust" }
+
+require('nvim-treesitter').install(language_list)
 require('nvim-treesitter').update()
+
+vim.api.nvim_create_autocmd('FileType', {
+	pattern = language_list,
+	callback = function()
+		-- syntax highlighting, provided by Neovim
+		vim.treesitter.start()
+		-- folds, provided by Neovim
+		vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+		vim.wo.foldmethod = 'expr'
+		-- indentation, provided by nvim-treesitter
+		vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+	end,
+})
