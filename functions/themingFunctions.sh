f0cd74caedadfbe427cb7168cd5981c714001f5c
Tino Heuberger <tinoheuberger@protonmail.com>
2026-01-13 22:06:07 +0100

    Also install gruvbox theme

--- functions/themingFunctions.sh
+++ functions/themingFunctions.sh
@@ -3,4 +3,5 @@
 SetupQTTheming() {
 	ReplaceInFile "~/.config/qt6ct/qt6ct.conf" "^style=.*" "style=kvantum"
 	InstallAurPackage "kvantum-theme-catppuccin-git" "https://aur.archlinux.org/kvantum-theme-catppuccin-git.git"
+	InstallAurPackage "kvantum-theme-gruvbox-git" "https://aur.archlinux.org/kvantum-theme-gruvbox-git.git"
 }
