650fdff079b1e74231438a9cc7a3f0fb10f0edab
Tino Heuberger <tinoheuberger@protonmail.com>
2026-02-21 12:02:42 +0100

    Adjust more settings according to nvim-lspconfig

--- defaults/.config/nvim/lua/lsp/ltex.lua
+++ defaults/.config/nvim/lua/lsp/ltex.lua
@@ -4,10 +4,6 @@ local root_files = {
 
 local home = os.getenv('HOME')
 
-local filetypes = {
-	'bib',
-	'tex',
-}
 
 local language_id_mapping = {
 	bib = 'bibtex',
@@ -19,32 +15,34 @@ local language_id_mapping = {
 	text = 'plaintext',
 }
 
-local function get_language_id(_, filetype)
-	local language_id = language_id_mapping[filetype]
-	if language_id then
-		return language_id
-	else
-		return filetype
-	end
-end
-local enabled_ids = {}
-do
-	local enabled_keys = {}
-	for _, ft in ipairs(filetypes) do
-		local id = get_language_id({}, ft)
-		if not enabled_keys[id] then
-			enabled_keys[id] = true
-			table.insert(enabled_ids, id)
-		end
-	end
-end
-
 vim.lsp.config('latex', {
 	name = "ltex_ls",
 	cmd = { "ltex-ls-plus" },
-	filetypes = filetypes,
+	filetypes = {
+		'asciidoc',
+		'bib',
+		'context',
+		'gitcommit',
+		'html',
+		'markdown',
+		'org',
+		'pandoc',
+		'plaintex',
+		'quarto',
+		'mail',
+		'mdx',
+		'rmd',
+		'rnoweb',
+		'rst',
+		'tex',
+		'text',
+		'typst',
+		'xhtml',
+	},
 	root_markers = root_files,
-	get_language_id = get_language_id,
+	get_language_id = function(_, filetype)
+		return language_id_mapping[filetype] or filetype
+	end,
 	settings = {
 		ltex = {
 			enabled = {
