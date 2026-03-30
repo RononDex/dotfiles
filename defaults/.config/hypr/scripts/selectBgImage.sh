ec89776dcf40345ec8b51af04baf7a42692e2a91
Tino Heuberger <tino.heuberger@fhnw.ch>
2026-03-30 14:28:36 +0200

    Actually generate color schema

--- defaults/.config/hypr/scripts/selectBgImage.sh
+++ defaults/.config/hypr/scripts/selectBgImage.sh
@@ -5,4 +5,5 @@ backgroundImage=$(find ${wallpapersDir} -type f | shuf -n 1) # select random bac
 mkdir -p ~/.cache/wal
 echo $backgroundImage >~/.cache/wal/bgImage
 
+wallust run $(cat ~/.cache/wal/bgImage)
 awww img $(cat ~/.cache/wal/bgImage) --transition-type right
