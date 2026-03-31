e059aed2b53730eed1f73b169ed6e0b5ff76e158
Tino Heuberger <tinoheuberger@protonmail.com>
2026-03-31 08:35:43 +0200

    Reload colors for kitty from generated template

--- defaults/.config/hypr/scripts/selectBgImage.sh
+++ defaults/.config/hypr/scripts/selectBgImage.sh
@@ -5,5 +5,6 @@ backgroundImage=$(find ${wallpapersDir} -type f | shuf -n 1) # select random bac
 mkdir -p ~/.cache/wal
 echo $backgroundImage >~/.cache/wal/bgImage
 
-wallust run $(cat ~/.cache/wal/bgImage)
+wallust run $(cat ~/.cache/wal/bgImage) -s
 awww img $(cat ~/.cache/wal/bgImage) --transition-type wipe --transition-angle 30 --transition-fps 60
+kill -SIGUSR1 $(pgrep kitty)
