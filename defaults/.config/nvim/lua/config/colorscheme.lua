f10ddecb4e0143616bae09504ae1946f4a72a6a5
Tino Heuberger <tinoheuberger@protonmail.com>
2026-04-01 21:44:19 +0200

    darken bg of nvim

--- defaults/.config/nvim/lua/config/colorscheme.lua
+++ defaults/.config/nvim/lua/config/colorscheme.lua
@@ -1,4 +1,10 @@
 require('onedark').setup {
-	style = 'darker'
+	style = 'darker',
+	colors = {
+		bg0 = "#16171a",
+		bg1 = "#1f2124",
+		bg2 = "#292c30",
+		bg3 = "#2b2d33",
+	}
 }
 require('onedark').load()
