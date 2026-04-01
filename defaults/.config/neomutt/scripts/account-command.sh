be7f58157587ae12fa034cbe4ef3d524de83b10c
Tino Heuberger <tinoheuberger@protonmail.com>
2026-04-01 23:42:49 +0200

    Bugfixes

--- defaults/.config/neomutt/scripts/account-command.sh
+++ defaults/.config/neomutt/scripts/account-command.sh
@@ -29,7 +29,7 @@ done
 if [[ $hostname == *"mail.hostpoint.ch"* ]] && [ "$username" = "tino.heuberger@sag-sas.ch" ]; then
 	decrypted=$(cat $scriptDir/../crypt-store/tino-sag-sas.hostpoint.ch | gpg --for-your-eyes-only --no-tty --quiet --decrypt)
 	echo "password: $decrypted"
-else if [[ $hostname == *"127.0.0.1"* ]] && [ "$username" = "tinoheuberger@protonmail.ch" ]; then
+elif [[ $hostname == *"127.0.0.1"* ]] && [ "$username" = "tinoheuberger@protonmail.com" ]; then
 	decrypted=$(cat $scriptDir/../crypt-store/tino-proton | gpg --for-your-eyes-only --no-tty --quiet --decrypt)
 	echo "password: $decrypted"
 fi
