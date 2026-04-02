3fff210729d52a5a4970b2106de28e046ece5e3d
Tino Heuberger <tinoheuberger@protonmail.com>
2026-04-02 17:48:43 +0200

    Dumb down config (seems broken currentlDumb down config (seems broken currently)y)

--- defaults/.config/nvim/lua/lsp/roslyn.lua
+++ defaults/.config/nvim/lua/lsp/roslyn.lua
@@ -1,133 +1,15 @@
 local lsp_utils = require("lsp.utils")
 
-local cmd = {
-	"roslyn",
-	"--stdio",
-	"--logLevel=Information",
-	"--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
-}
-
 local group = vim.api.nvim_create_augroup('lspconfig.roslyn_ls', { clear = true })
 
----@param client vim.lsp.Client
-local function refresh_diagnostics(client)
-	for buf, _ in pairs(vim.lsp.get_client_by_id(client.id).attached_buffers) do
-		if vim.api.nvim_buf_is_loaded(buf) then
-			client:request(
-				vim.lsp.protocol.Methods.textDocument_diagnostic,
-				{ textDocument = vim.lsp.util.make_text_document_params(buf) },
-				nil,
-				buf
-			)
-		end
-	end
-end
-
-local function roslyn_handlers()
-	return {
-		['workspace/projectInitializationComplete'] = function(_, _, ctx)
-			vim.notify('Roslyn project initialization complete', vim.log.levels.INFO, { title = 'roslyn_ls' })
-			local client = assert(vim.lsp.get_client_by_id(ctx.client_id))
-			refresh_diagnostics(client)
-			return vim.NIL
-		end,
-		['workspace/_roslyn_projectNeedsRestore'] = function(_, result, ctx)
-			local client = assert(vim.lsp.get_client_by_id(ctx.client_id))
-
-			---@diagnostic disable-next-line: param-type-mismatch
-			client:request('workspace/_roslyn_restore', result, function(err, response)
-				if err then
-					vim.notify(err.message, vim.log.levels.ERROR, { title = 'roslyn_ls' })
-				end
-				if response then
-					for _, v in ipairs(response) do
-						vim.notify(v.message, vim.log.levels.INFO, { title = 'roslyn_ls' })
-					end
-				end
-			end)
-
-			return vim.NIL
-		end,
-		['razor/provideDynamicFileInfo'] = function(_, _, _)
-			vim.notify(
-				'Razor is not supported.\nPlease use https://github.com/tris203/rzls.nvim',
-				vim.log.levels.WARN,
-				{ title = 'roslyn_ls' }
-			)
-			return vim.NIL
-		end,
-	}
-end
-
-vim.filetype.add({
-	extension = {
-		razor = "razor",
-		cshtml = "razor",
-	},
-})
-
 vim.lsp.config("roslyn", {
-	name = "roslyn",
-	cmd = cmd,
-	offset_encoding = 'utf-8',
-	filetypes = {
-		"razor",
-		"cs",
-	},
-	commands = {
-		['roslyn.client.completionComplexEdit'] = function(command, ctx)
-			local client = assert(vim.lsp.get_client_by_id(ctx.client_id))
-			local args = command.arguments or {}
-			local uri, edit = args[1], args[2]
-
-			---@diagnostic disable: undefined-field
-			if uri and edit and edit.newText and edit.range then
-				local workspace_edit = {
-					changes = {
-						[uri.uri] = {
-							{
-								range = edit.range,
-								newText = edit.newText,
-							},
-						},
-					},
-				}
-				vim.lsp.util.apply_workspace_edit(workspace_edit, client.offset_encoding)
-				---@diagnostic enable: undefined-field
-			else
-				vim.notify('roslyn_ls: completionComplexEdit args not understood: ' .. vim.inspect(args),
-					vim.log.levels.WARN)
-			end
-		end,
-	},
-	handlers = roslyn_handlers(),
-	capabilities = {
-		-- HACK: Doesn't show any diagnostics if we do not set this to true
-		textDocument = {
-			diagnostic = {
-				dynamicRegistration = true,
-			},
-		},
-	},
 	on_attach = function(client, bufnr)
 		lsp_utils.default_on_attach(client, bufnr)
 		-- avoid duplicate autocmds for same buffer
 		if vim.api.nvim_get_autocmds({ buffer = bufnr, group = group })[1] then
 			return
 		end
-
-		vim.api.nvim_create_autocmd({ 'BufWritePost', 'InsertLeave' }, {
-			group = group,
-			buffer = bufnr,
-			callback = function()
-				if client then
-					refresh_diagnostics(client)
-				end
-			end,
-			desc = 'roslyn_ls: refresh diagnostics',
-		})
 	end,
-	root_markers = { '.git', '.sln', '.csproj' },
 	settings = {
 		["csharp|inlay_hints"] = {
 			csharp_enable_inlay_hints_for_implicit_object_creation = true,
