9f96bf7bac20be9d5553f6219afc66d33f5e36b7
Tino Heuberger <tinoheuberger@protonmail.com>
2026-04-02 09:48:50 +0200

    Typo

--- functions/packageFunctions.sh
+++ functions/packageFunctions.sh
@@ -15,13 +15,14 @@ SetupClamAV() {
 	sudo mkdir -p /etc/clamav && sudo cp ~/.files/clamav/clamd.conf /etc/clamav/clamd.conf
 	sudo mkdir -p /etc/clamav && sudo cp ~/.files/clamav/notify.sh /etc/clamav/notify.sh
 	sudo cp ~/.files/clamav/sudoers-config /etc/sudoers.d/clamav
-	sudo chmod +x /etc/clamav/clamav/notify.sh
+	sudo chmod +x /etc/clamav/notify.sh
 	sudo mkdir -p /etc/systemd/system/clamav-clamonacc.service.d && sudo cp ~/.files/clamav/clmonacc.service-override /etc/systemd/system/clamav-clamonacc.service.d/override.conf
 
 	sudo mkdir -p /var/log/clamav
 	sudo touch /var/log/clamav/freshclam.log
 	sudo chmod 600 /var/log/clamav/freshclam.log
 	sudo chown clamav /var/log/clamav/freshclam.log
+	sudo chown -R clamav /etc/clamav/certs
 
 	sudo systemctl enable clamav-clamonacc.service
 	sudo systemctl restart clamav-clamonacc.service
