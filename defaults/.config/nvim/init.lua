18d9aba34303b99f6ac93c0e60e1d07c37608ac8
Tino Heuberger <tino.heuberger@fhnw.ch>
2026-04-09 15:49:22 +0200

    Setup according to docs

--- defaults/.config/nvim/init.lua
+++ defaults/.config/nvim/init.lua
@@ -40,6 +40,7 @@ require("lsp.json")
 require("lsp.openscad")
 require("lsp.clangd")
 require("lsp.harper")
+require("lsp.roslyn")
 vim.lsp.enable({ "jdtls", "lua", "go", "ltex_ls", "xml", "pyright", "bashls", "phpactor", "html",
 	"css", "texlab",
 	"javascript", "json", "openscad", "clangd", --[[ "harper" ]] })
