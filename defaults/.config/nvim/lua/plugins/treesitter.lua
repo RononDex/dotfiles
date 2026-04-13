ed2f0cc6be31110f272e5d34f3c53191a454628b
Tino Heuberger <tinoheuberger@protonmail.com>
2026-04-13 16:03:44 +0200

    Switch to neovim treesitter

--- defaults/.config/nvim/lua/plugins/treesitter.lua
+++ defaults/.config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 return {
-	'nvim-treesitter/nvim-treesitter',
+	'neovim-treesitter/nvim-treesitter',
 	dependencies = { 'neovim-treesitter/treesitter-parser-registry' },
 	lazy = false,
 	build = ':TSUpdate',
