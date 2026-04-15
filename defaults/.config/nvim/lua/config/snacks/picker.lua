91fedcae39cd567be6012a39b3001a70961fcba5
Tino Heuberger <tinoheuberger@protonmail.com>
2026-04-15 10:34:00 +0200

    Disable smooth scrolling for now

--- defaults/.config/nvim/lua/config/snacks/picker.lua
+++ defaults/.config/nvim/lua/config/snacks/picker.lua
@@ -2,19 +2,14 @@ return {
 	enabled = true,
 	sources = {
 		explorer = {
-			auto_close = true,
-			layout = function()
-				return {
-					preset = 'sidebar',
-					preview = false,
-					layout = {
-						width = (vim.g.explorer_size or {}).width,
-					},
-				}
-			end,
-			on_close = function(picker)
-				vim.g.explorer_size = picker.layout.root:size()
-			end,
+			auto_close = false,
+			layout = {
+				preset = 'sidebar',
+				preview = false,
+				layout = {
+					width = 25,
+				},
+			},
 		}
 	}
 }
--- defaults/.config/nvim/lua/plugins/snacks.lua
+++ defaults/.config/nvim/lua/plugins/snacks.lua
@@ -16,7 +16,7 @@ return {
 		notifier = { enabled = true },
 		quickfile = { enabled = true },
 		scope = { enabled = true },
-		scroll = { enabled = true },
+		scroll = { enabled = false },
 		statuscolumn = { enabled = true },
 		words = { enabled = true },
 	},
