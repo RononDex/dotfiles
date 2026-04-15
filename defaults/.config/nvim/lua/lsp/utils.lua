79774ad321fcf30c32a1c0d2d9d45132b2792874
Tino Heuberger <tinoheuberger@protonmail.com>
2026-04-15 14:15:03 +0200

    Remove obsolete calls

--- defaults/.config/nvim/lua/lsp/utils.lua
+++ defaults/.config/nvim/lua/lsp/utils.lua
@@ -1,23 +1,19 @@
 return {
 	default_on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/inlayHint") then
-			if vim.lsp.inlay_hint then
-				vim.lsp.inlay_hint.enable(true)
-			end
+		if vim.lsp.inlay_hint then
+			vim.lsp.inlay_hint.enable(true)
 		end
-		if client.supports_method("textDocument/codeLens") then
-			vim.lsp.codelens.enable(true)
+		vim.lsp.codelens.enable(true)
 
-			local codelense_cmds = vim.api.nvim_create_augroup('codelense_cmds', { clear = true })
+		local codelense_cmds = vim.api.nvim_create_augroup('codelense_cmds', { clear = true })
 
-			vim.api.nvim_create_autocmd('BufWritePost', {
-				buffer = bufnr,
-				group = codelense_cmds,
-				desc = 'refresh codelens',
-				callback = function()
-					vim.lsp.codelens.enable(true)
-				end,
-			})
-		end
+		vim.api.nvim_create_autocmd('BufWritePost', {
+			buffer = bufnr,
+			group = codelense_cmds,
+			desc = 'refresh codelens',
+			callback = function()
+				vim.lsp.codelens.enable(true)
+			end,
+		})
 	end
 }
