b3419b97c8b23c4098b658cc27bd25b7975ec33e
Tino Heuberger <tino.heuberger@fhnw.ch>
2026-04-16 13:54:05 +0200

    More deprecation fixes

--- defaults/.config/nvim/lua/utils.lua
+++ defaults/.config/nvim/lua/utils.lua
@@ -4,9 +4,9 @@ local utils = {
 }
 
 function utils.is_editable()
-	local current_buffer = vim.fn.bufnr("%")
-	local is_modifiable = vim.api.nvim_buf_get_option(current_buffer, "modifiable")
-	local is_readonly = vim.api.nvim_buf_get_option(current_buffer, "readonly")
+	local current_buffer = vim.api.nvim_get_current_buf()
+	local is_modifiable = vim.api.nvim_get_option_value("modifiable", { scope = "local", buf = current_buffer })
+	local is_readonly = vim.api.nvim_get_option_value("readonly", { scope = "local", buf = current_buffer })
 	return is_modifiable and not is_readonly
 end
 
