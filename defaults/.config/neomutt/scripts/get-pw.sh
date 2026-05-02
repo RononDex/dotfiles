01e7d5154f0fdb95692aa5cd5520a3bd8f808afa
Tino Heuberger <tinoheuberger@protonmail.com>
2026-05-02 23:55:49 +0200

    Use mailBox var

--- defaults/.config/neomutt/scripts/get-pw.sh
+++ defaults/.config/neomutt/scripts/get-pw.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mailBox=$1
-gpg --for-your-eyes-only --pinentry-mode loopback --default-key MachineKey --passphrase "$(cat /etc/hostname):$(cat /etc/machine-id)" --no-tty --quiet --decrypt ~/.config/neomutt/crypt-store/$1
+gpg --for-your-eyes-only --pinentry-mode loopback --default-key MachineKey --passphrase "$(cat /etc/hostname):$(cat /etc/machine-id)" --no-tty --quiet --decrypt ~/.config/neomutt/crypt-store/$mailBox
