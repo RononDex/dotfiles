1fa7543ea50ae647050e5b5400170f12ea663d93
Tino Heuberger <tinoheuberger@protonmail.com>
2026-05-02 23:23:28 +0200

    Use full quotes

--- functions/securityFunctions.sh
+++ functions/securityFunctions.sh
@@ -8,7 +8,7 @@ SetupMachineGpgKey() {
 		echo "Creating machine specific gpg key"
 		pass_phrase="$host_name:$(cat /etc/machine-id)"
 		key_name="MachineKey <machine@$host_name>"
-		gpg --batch --pinentry-mode loopback --passphrase '$pass_phrase' --quick-gen-key "$key_name" ed25519 default 0 &&
+		gpg --batch --pinentry-mode loopback --passphrase "$pass_phrase" --quick-gen-key "$key_name" ed25519 default 0 &&
 			gpg --batch --pinentry-mode loopback --passphrase '$pass_phrase' --quick-add-key "$(gpg --list-keys --with-colons "$key_name" | awk -F: '/^fpr/{print $10; exit}')" cv25519 encr 0
 	fi
 }
