03e6fa4d42bf5fba302fd8a6c47aef27c7aed3c3
Tino Heuberger <tinoheuberger@protonmail.com>
2026-05-06 09:15:31 +0200

    Attempt to fix hop in pager

--- defaults/.config/nvimpager/init.lua
+++ defaults/.config/nvimpager/init.lua
@@ -1,3 +1,7 @@
 -- Load hop from nvim installation
 vim.opt.rtp:append(vim.fn.expand('~/.local/share/nvim/lazy/hop.nvim'))
-vim.opt.rtp:append(vim.fn.expand('~/.config/nvim/lua/config/hop.nvim'))
+
+require('hop').setup({
+	keys = 'etovxqpdygfblzhckisuran',
+})
+vim.keymap.set("n", " ", "<cmd>HopWord<cr>")
