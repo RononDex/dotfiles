9174fc6b435c9caa2d802b3a6c2a1737ee0cbada
Tino Heuberger <tinoheuberger@protonmail.com>
2026-05-06 08:42:10 +0200

    Only show virtual lines for WARN and above

--- defaults/.config/nvim/lua/config/diagnostics.lua
+++ defaults/.config/nvim/lua/config/diagnostics.lua
@@ -1,1 +1,8 @@
-vim.diagnostic.config({ virtual_lines = true })
+vim.diagnostic.config({
+	virtual_lines = {
+		severity = { min = vim.diagnostic.severity.WARN },
+	},
+	underline = {
+		severity = { min = vim.diagnostic.severity.HINT },
+	},
+})
