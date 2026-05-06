151b183db423e6c060cc7f11507c091293b0de84
Tino Heuberger <tinoheuberger@protonmail.com>
2026-05-06 09:43:31 +0200

    Some more generic settings

--- defaults/.config/nvimpager/init.lua
+++ defaults/.config/nvimpager/init.lua
@@ -1,4 +1,21 @@
 -- Load hop from nvim installation
+-- Has to be defined before any other settings
+vim.g.mapleader = ','
+vim.g.maplocalleader = ','
+
+--vim.opt.background = 'dark'
+vim.opt.clipboard = 'unnamedplus' -- Use global clipboard
+vim.opt.number = true             -- Show LineNumbers
+vim.opt.smartcase = true
+vim.opt.mouse = 'a'               -- Enable all mouse features
+vim.opt.tabstop = 4
+vim.opt.termguicolors = true
+--Save undo history
+vim.opt.undofile = true
+
+--Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
 
 vim.opt.rtp:append(vim.fn.expand('~/.local/share/nvim/lazy/hop.nvim'))
 
