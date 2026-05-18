fc732c6e0904635c06ed7447251e7cd743adef63
Tino Heuberger <tino.heuberger@fhnw.ch>
2026-05-18 16:10:00 +0200

    Install systemd timers for automatic backups

--- profiles/pollux/setup.sh
+++ profiles/pollux/setup.sh
@@ -4,3 +4,7 @@ scriptDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 sudo rm -rf /root/scripts
 sudo cp -r $scriptDir/scripts /root/scripts
+
+sudo chmod -R +x /root/scripts/
+sudo cp $scriptDir/restic-backup.service /etc/systemd/system/restic-backup.service
+sudo cp restic-backup.timer /etc/systemd/system/restic-backup.timer
