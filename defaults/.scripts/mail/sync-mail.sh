d1027b15966803aad699b3c9041d0206f6743804
Tino Heuberger <tinoheuberger@protonmail.com>
2026-05-20 16:24:30 +0200

    Use mailsync to also sync with vdirsyncer

--- defaults/.scripts/mail/sync-mail.sh
+++ defaults/.scripts/mail/sync-mail.sh
@@ -11,6 +11,10 @@ if [ -n "$MBSYNC" -o -n "$NOTMUCH" ]; then
 	exit 0
 fi
 
+echo "Syncing with vdirsyncer ..."
+y | vdirsyncer discover
+vdirsyncer sync
+
 echo "Deleting messages tagged as *deleted*"
 notmuch search --format=text0 --output=files tag:deleted | xargs -0 --no-run-if-empty rm -v
 
