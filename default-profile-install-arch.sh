db3412ad521565e1cf68a8a24478e4cf7c74a315
Tino Heuberger <tino.heuberger@fhnw.ch>
2026-06-15 11:24:43 +0200

    Remote -e for now

--- default-profile-install-arch.sh
+++ default-profile-install-arch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -euo pipefail
+set -uo pipefail
 
 scriptDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
--- profile-enabler.sh
+++ profile-enabler.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euo pipefail
+set -uo pipefail
 
 scriptDirRoot="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 cd "$scriptDirRoot"
