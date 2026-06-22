bd8edd1d4d6f59aa057ef3b12c332e94f4b4ba3f
Tino Heuberger <tino.heuberger@fhnw.ch>
2026-06-22 16:27:31 +0200

    Allow fuzzy matching

--- defaults/.config/aerc/autocomplete.sh
+++ defaults/.config/aerc/autocomplete.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 QUERY="$1"
-GROUPS=~/Nextcloud/mail-aliases.conf
+GROUPS=~/.config/aerc/groups
 
-# Check if query exactly matches a group name
-MATCH=$(grep -i "^$QUERY=" "$GROUPS" | cut -d'=' -f2)
+# Find all groups starting with the query
+while IFS='=' read -r name emails; do
+	case "$name" in
+	$QUERY*) printf "%s\tGroup: %s\n" "$emails" "$name" ;;
+	esac
+done <"$GROUPS"
 
-if [ -n "$MATCH" ]; then
-	printf "%s\tGroup: %s\n" "$MATCH" "$QUERY"
-else
-	khard email --parsable --remove-first-line "$QUERY"
-fi
+# Also do normal khard lookup
+khard email --parsable "$QUERY"
