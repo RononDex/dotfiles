7365bc34ae6314a3f04502348a34e41d80e4194b
Tino Heuberger <tinoheuberger@protonmail.com>
2026-06-23 10:43:37 +0200

    First line does not need to be removed

--- defaults/.config/aerc/scripts/group-adresses.sh
+++ defaults/.config/aerc/scripts/group-adresses.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mailAdresses=$(khard email --parsable --remove-first-line $1 | awk -F'\t' 'NR>1 {print $1}' | paste -sd,)
+mailAdresses=$(khard email --parsable $1 | awk -F'\t' 'NR>1 {print $1}' | paste -sd,)
 aerc :header To "$mailAdresses"
