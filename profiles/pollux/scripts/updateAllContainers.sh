b3b2e859873221ca9ca0a58f703de861c8b956f8
Tino Heuberger <tinoheuberger@protonmail.com>
2026-08-15 21:56:47 +0200

    Add missing echo

--- profiles/pollux/scripts/updateAllContainers.sh
+++ profiles/pollux/scripts/updateAllContainers.sh
@@ -4,6 +4,10 @@ exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>/root/logs/updateAllContainers.log 2>&1
 
+echo "--------------------------------------------"
+echo " Updating all containers at $(date)"
+echo "--------------------------------------------"
+
 bash /root/scripts/updateMeteorastronomie.sh
 bash /root/scripts/updateUptimeKuma.sh
 bash /root/scripts/updateNextcloud.sh
