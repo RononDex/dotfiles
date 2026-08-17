5b060851ea6be632acbce54f545bdbf13ecd8c92
Tino Heuberger <tinoheuberger@protonmail.com>
2026-08-17 11:45:15 +0200

    Setup scripts for auto updating

--- profiles/ATLANTIS-Server/profile-enabler.sh
+++ profiles/ATLANTIS-Server/profile-enabler.sh
@@ -31,7 +31,7 @@ sudo mkdir -p /etc/samba
 sudo cp $scriptDir/overrides/samba/smb.conf /etc/samba/smb.conf
 sudo mkdir -p /data
 sudo cp ~/.scripts/utilities/updateAndReboot /root/updateAndReboot
-sudo cp $scriptDir/overrides/scripts/doBackup /root/doBackup
+sudo cp $scriptDir/overrides/scripts/ /root/scripts
 sudo cp ~/.scripts/backup/backupResticServer /root/backupResticServer
 
 sudo cp $scriptDir/overrides/sysctl.conf /etc/sysctl.conf
