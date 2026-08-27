59a5d3a61cdfb94ed51762cd3af31cba45eb8f3d
Tino Heuberger <tinoheuberger@protonmail.com>
2026-08-27 08:57:19 +0200

    Remove neo tree reference (no longer installed)

--- defaults/.config/nvim/lua/config/debugging.lua
+++ defaults/.config/nvim/lua/config/debugging.lua
@@ -8,10 +8,6 @@ local utils = require("utils")
 
 dap.listeners.after.event_initialized["dapui_config"] = function()
 	dapui.open()
-	if utils.is_neotree_open() then
-		utils.neotree_open_before_debug = true
-		vim.cmd("Neotree close")
-	end
 end
 dap.listeners.before.event_terminated["dapui_config"] = function()
 	dapui.close()
