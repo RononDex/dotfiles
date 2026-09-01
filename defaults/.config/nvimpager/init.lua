03b6f71608be9a616b5fd689b925f06158711bce
Tino Heuberger <tino.heuberger@fhnw.ch>
2026-09-01 09:23:26 +0200

    Remove "f" used for email forwarding

--- defaults/.config/nvimpager/init.lua
+++ defaults/.config/nvimpager/init.lua
@@ -35,7 +35,7 @@ window.clip_line_context = function(line_ctx, win_ctx)
 end
 
 require('hop').setup({
-	keys = 'etvxpdygfblzhckisuan',
+	keys = 'etvxpdygblzhckisuan',
 })
 local function set_hop_map()
 	vim.keymap.set("n", " ", "<cmd>HopWord<cr>", { buffer = true, nowait = true })
